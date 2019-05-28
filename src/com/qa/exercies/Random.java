f4205580c4c6bf9124444b4611f5796a4309e581
HughH1 <hughfharding@gmail.com>
2019-05-28 17:36:11 +0100

    math random added

--- src/com/qa/exercies/Random.java
+++ src/com/qa/exercies/Random.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Random {
 
 	public static void main(String[] args) {
-		int x = ThreadLocalRandom.current().nextInt(30, 40);
-		System.out.println(x);
+		int x = ThreadLocalRandom.current().nextInt(10, 40);
+		double y = ThreadLocalRandom.current().nextDouble(10.1, 40.3);
+		System.out.println(y);
 	}
 }
